779d26e103d7cebd8f5f9566c4306ce86f2331bd
daswedishchef <spencerthecarlson@gmail.com>
2021-04-29 14:10:56 -0700

    Functional but not optimal

--- asn2/Players.py
+++ asn2/Players.py
@@ -4,6 +4,7 @@
     If using this code please cite creator.
 
 '''
+import numpy as np
 
 class Player:
     def __init__(self, symbol):
@@ -13,11 +14,44 @@ class Player:
     def get_symbol(self):
         return self.symbol
     
+    def get_actions(self, board, symbol):
+        actions = list()
+        print(symbol)
+        for col in range(4):
+            for row in range(4):
+                if(board.is_legal_move(col, row, symbol)):
+                    actions.append((col, row))
+        return actions
+
     #parent get_move should not be called
+    # def get_move(self, board):
+    #     #get available actions and successor states
+    #     s = list()
+    #     a = self.get_actions(board, self.symbol)
+    #     for ch in a:
+    #         tb = board.cloneOBoard()
+    #         tb.play_move(ch[0], ch[1], self.symbol)
+    #         s.append(tb.count_score(self.symbol))
+    #     r = np.argmin(s)
+    #     return a[r]
+            
     def get_move(self, board):
-        raise NotImplementedError()
-
-
+        #get available actions and successor states
+        rs = list()
+        a = self.get_actions(board, self.symbol)
+        for ch in a:
+            s = list()
+            tb = board.cloneOBoard()
+            tb.play_move(ch[0], ch[1], self.symbol)
+            sa = self.get_actions(board, board.p1_symbol)
+            for act in sa:
+                ts = board.cloneOBoard()
+                ts.play_move(act[0], act[1], board.p1_symbol)
+                s.append(ts.count_score(board.p1_symbol))
+            rs.append(np.argmax(s))
+        r = np.argmin(rs)
+        return a[r]
+           
 
 class HumanPlayer(Player):
     def __init__(self, symbol):
