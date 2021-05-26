066443673f56c6e44b4cfeb5f7df236f619d900b
daswedishchef <spencerthecarlson@gmail.com>
2021-05-25 20:55:44 -0700

    finished

--- asn3/bayes.py
+++ asn3/bayes.py
@@ -1,3 +1,7 @@
+#Authors
+#Spencer Carlson
+#Sheng Tse Tsai
+
 import numpy as np
 
 junk = ("'", ",", ".", "!", "?", "(", ")", "-", "\n", ":", ";", "/", "*")
@@ -63,65 +67,71 @@ def label(data):
         labels.append(d[-1])
     return evidence, labels
 
-def classy(data, test):
+def classy(data, test, vocab):
+    #separate labels and data points
     evidence, labels = label(data)
     ntrue = 0
     nfalse = 0
     p_true = list()
     p_false = list()
+    #initiate lists to zero for probabilities of each feature
     for j in range(len(evidence[0])):
         p_true.append(0)
         p_false.append(0)
+    #for each case
     for i in range(len(labels)):
         if labels[i] == 1:
             ntrue += 1
+            #if positive sum instances
             for f in range(len(evidence[i])):
                 p_true[f] += evidence[i][f] 
         else:
+            #if negative sum instances
             nfalse += 1
             for f in range(len(evidence[i])):
                 p_false[f] += evidence[i][f]
     #P(word|class)
     for i in range(len(p_false)):
-        p_true[i] += 1
-        p_true[i] /= (ntrue + 2)
         p_false[i] += 1
         p_false[i] /= (nfalse + 2)
-    #P(true)
-    pT = ntrue/(ntrue + nfalse)
-    #P(false)
-    # pF = 1-pT
-    pF = nfalse/(ntrue + nfalse)
-    test_data, Tlabels = label(test)
+    for i in range(len(p_true)):
+        p_true[i] += 1
+        p_true[i] /= (ntrue + 2)
+    #separate test data and labels
+    testRaw = featureGen(vocab, test)
+    test_data, Tlabels = label(testRaw)
     results = list()
+    #for each test case
     for i in range(len(test_data)):
-        Tprob = -1.0
-        Fprob = -1.0
+        #P(true)
+        pT = np.log((ntrue + 1)/(ntrue + nfalse + 2))
+        #P(false)
+        # pF = 1-pT
+        pF = np.log((nfalse + 1)/(ntrue + nfalse + 2))
         for j in range(len(test_data[i])):
+            #word exists
             if test_data[i][j] == 1:
-                if Tprob == -1.0:
-                    Tprob = np.log(p_true[j])
-                else:
-                    Tprob *= np.log(p_true[j])
-            # else:
-                if Fprob == -1.0:
-                    Fprob = np.log(1-p_false[j])
-                else:
-                    Fprob *= np.log(1- p_false[j])
-        Fprob *= pF
-        Tprob *= pT
-        if Fprob > Tprob:
+                pT += np.log(p_true[j])
+                pF += np.log(p_false[j])
+            #word does not exist
+            else:
+                pF += np.log(1-p_false[j])
+                pT += np.log(1-p_true[j])
+        #if probability of 0 is higher, add to results
+        if pF > pT:
             results.append(0)
         else:
             results.append(1)
     acc = 0
     index = 0
+    #calculate accuracy
     for i in range(len(results)):
         if results[i] == Tlabels[i]:
             acc += 1
         index += 1
     accuracy = acc/index
     return results, accuracy
+
 
 
 def output_file(name, vocab, features):
@@ -141,7 +151,17 @@ def main():
     train_vocab = prepro[3]
     F_test = featureGen(prepro[2], prepro[0])
     F_train = featureGen(prepro[3], prepro[1])
-    print(classy(F_train, F_train))
+    training = classy(F_train, train_cases, train_vocab)
+    testing = classy(F_train, test_cases, train_vocab)
+    testOut = open("results.txt", 'w')
+    testOut.write("Training Results\n")
+    testOut.write("Accuracy: ")
+    testOut.write(str(training[1]))
+    testOut.write("\n")
+    testOut.write("Test Results\n")
+    testOut.write("Accuracy: ")
+    testOut.write(str(testing[1]))
+    testOut.write("\n")
     output_file("preprocessed_train.txt", train_vocab, F_train)
     output_file("preprocessed_test.txt", test_vocab, F_test)
 
